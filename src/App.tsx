de56645b062785be9eae38a893a8a55a7e30e7b7
ErnestoPelayo <ernesto.p@itmexicali.edu.mx>
2024-07-16 17:12:01 -0700

    added template

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,45 @@
 
+import '../src/index.css'
 
 function App() {
 
   return (
     <>
-      Guitar La 
+      <header className="py-5 header">
+        <div className="container-xl">
+            <div className="row justify-content-center justify-content-md-between">
+                <div className="col-8 col-md-3">
+                    <a href="index.html">
+                        <img className="img-fluid" src="./public/img/logo.svg" alt="imagen logo" />
+                    </a>
+                </div>
+                <nav className="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
+                    <div 
+                        className="carrito"
+                    >
+                        <img className="img-fluid" src="./public/img/carrito.png" alt="imagen carrito" />
+                        {/* Aqui va el carrito de compras */}
+                        
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </header>
+
+    <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+
+        <div className="row mt-5">
+            {/* Aqui va las guitarras */}
+        </div>
+    </main>
+
+
+    <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+            <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+        </div>
+    </footer> 
     </>
   )
 }
