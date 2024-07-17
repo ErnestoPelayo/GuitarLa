3afa0ced0fbbd982a3c72447e6277f289951dab9
ErnestoPelayo <ernesto.p@itmexicali.edu.mx>
2024-07-16 18:45:04 -0700

    cartItem created

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,12 @@
+
 export type guitarItem = {
     id : number,
     name:string,
     image:string,
     description:string,
     price:number
+}
+
+export type cartItem  = guitarItem & {
+    quantity : number
 }
