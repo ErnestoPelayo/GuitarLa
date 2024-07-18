2704c49c2ad89fe66f2318aad740543635e9e6a0
ErnestoPelayo <ernesto.p@itmexicali.edu.mx>
2024-07-17 20:19:00 -0700

    guitarItem modified

--- src/components/GuitarItem.tsx
+++ src/components/GuitarItem.tsx
@@ -1,24 +1,26 @@
+import { Dispatch } from "react"
+import { guitarItem } from "../types"
+import { CartActions } from "../reducers/cart-reducer"
 
-type guitarItemProps = {
-    image : string,
-    name : string,
-    price : number,
-    description : string
+type GuitarProps = {
+  guitar : guitarItem, 
+  dispatch: Dispatch<CartActions>
 }
 
-const GuitarItem = ({image,name,price,description}:guitarItemProps) => {
+const GuitarItem = ({guitar,dispatch}:GuitarProps) => {
   return (
     <div className="col-md-6 col-lg-4 my-4 row align-items-center">
                 <div className="col-4">
-                    <img className="img-fluid" src={`/img/${image}.jpg`} alt="imagen guitarra" />
+                    <img className="img-fluid" src={`/img/${guitar.image}.jpg`} alt="imagen guitarra" />
                 </div>
                 <div className="col-8">
-                    <h3 className="text-black fs-4 fw-bold text-uppercase">{name}</h3>
-                    <p>{description}</p>
-                    <p className="fw-black text-primary fs-3">${price}</p>
+                    <h3 className="text-black fs-4 fw-bold text-uppercase">{guitar.name}</h3>
+                    <p>{guitar.description}</p>
+                    <p className="fw-black text-primary fs-3">${guitar.price}</p>
                     <button 
                         type="button"
                         className="btn btn-dark w-100"
+                        onClick={()=>dispatch({type:"add-to-cart",payload:{item:guitar}})}
                     >Agregar al Carrito</button>
                 </div>
     </div>
